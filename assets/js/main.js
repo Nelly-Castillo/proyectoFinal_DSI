c8de810073bca61307f59447d02b2c2181ec9460
Ruth Mtz <99702831+RuthMitty@users.noreply.github.com>
2023-11-25 16:29:46 -0600

    Se añadió la funcionalidad del me gusta y se llama al script de js hasta el final del index

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
-let corazon = document.getElementById('megusta');
-corazon.addEventListener("click", (e)=>{
+const corazon = document.getElementById("megusta");
+corazon.addEventListener("click", (e) => {
     darLike(e);
+    // console.log('Se dió like');
 });
 
 function darLike(evento){
-    evento.target.classList.toggle("bi-heart");
     evento.target.classList.toggle("bi-heart-fill");
+    evento.target.classList.toggle("bi-heart");
+    
 }
